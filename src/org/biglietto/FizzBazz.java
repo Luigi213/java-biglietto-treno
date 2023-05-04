7ab6a528574b272bc2f8b9ced8499a37fb1b8e3d
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-04 17:14:24 +0200

    add fizzbazz

---
+++ src/org/biglietto/FizzBazz.java
@@ -0,0 +1,28 @@
+package org.biglietto;
+
+import java.util.Scanner;
+
+public class FizzBazz {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Quanti numeri da stampare");
+		int num = in.nextInt();
+		
+		System.out.println("Ecco i numeri:");
+		
+		for(int i=1; i<=num; i++) {
+			if(i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if(i % 5 == 0) {
+				System.out.println("Bazz");
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+		
+		in.close();
+	}
+}
