cbea65f3db9f98c04ce24927e038f20c67c65c29
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-04 14:55:05 +0200

    completed

--- src/org/biglietto/JavaBiglietto.java
+++ src/org/biglietto/JavaBiglietto.java
@@ -1,5 +1,36 @@
 package org.biglietto;
 
-public class JavaBiglietto {
+import java.util.Scanner;
 
+public class JavaBiglietto {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserire i kolimetri");
+		int km = in.nextInt();
+		
+		System.out.println("Inserire l'eta'");
+		int age = in.nextInt();
+		
+		discount(km, age);
+		
+		in.close();
+		
+	}
+	
+	public static void discount(int km, int age) {
+		if(age < 18) {
+			double price = km * 0.8 * 0.21; 
+			System.out.println("Il prezzo del biglietto: " + price + " €");
+		}
+		else if(age > 65) {
+			double price = km * 0.6 * 0.21; 
+			System.out.println("Il prezzo del biglietto: " + price + " €");
+		}
+		else {
+			double price = km * 0.21; 
+			System.out.println("Il prezzo del biglietto: " + price + " €");
+		}
+	}
 }
