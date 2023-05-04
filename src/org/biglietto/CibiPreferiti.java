2376077d54732816cb4a05cadf8974503ddccd52
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-04 16:59:59 +0200

    update cibi preferiti

--- src/org/biglietto/CibiPreferiti.java
+++ src/org/biglietto/CibiPreferiti.java
@@ -2,7 +2,7 @@ package org.biglietto;
 
 public class CibiPreferiti {
 	public static void main(String[] args) {
-		String[] arrFood = {"lasagna", "pizza", "gelato", "kebab", "carbonara"};
+		String[] arrFood = {"lasagna", "pizza", "gelato", "kebab", "carbonara", "ciao"};
 		
 		System.out.println("lunghezza della classifica: " + arrFood.length);
 		for(int i=0; i<arrFood.length; i++) {
@@ -11,9 +11,15 @@ public class CibiPreferiti {
 			} else if(i == arrFood.length - 1 ) {
 				System.out.println("il mio cibo meno preferito della classifica: " + arrFood[i]);
 			}
-			else {
-				System.out.println("il cibo in mezzo della classifica: " + arrFood[i]);
-			}
+		}
+		
+		int media = arrFood.length / 2;
+		
+		System.out.println("mediano " + arrFood[media]);
+		
+		if(arrFood.length % 2 == 0) {
+			System.out.println("mediano " + arrFood[media - 1]);
+			System.out.println("mediano " + arrFood[media]);
 		}
 	}
 }
